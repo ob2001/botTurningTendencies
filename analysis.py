502bed00b5fe42ed9710a5cbc6286868e7950913
Owen <butlero2001@outlook.com>
2022-06-13 14:31:24 -0400

    Can now trim parts of paths too far out radially

--- analysis.py
+++ analysis.py
@@ -1,22 +1,38 @@
 from mylib import *
 import matplotlib.pyplot as plt
 
+def trimradius(X, Y, R, val):
+    newarr = [[x, y] for x, y, r in zip(X, Y, R) if r < val]
+    return np.array(newarr).transpose()
+
 args = arguments()
 n, headers, data = readbotfile(args.botnum)
 mheaders1, mheaders2, measurements, botdict = readvolts()
 
 """ Plotting """
+# Plot trajectory of chosen bot for all 1-minute periods
 if(args.traj):
-    for i in range(n):
-        fig = plt.figure(figsize = (17, 8))
-        ax1 = fig.add_subplot(121)
-        ax2 = fig.add_subplot(122, projection = "polar")
-        ax1.plot(data[i,:,2], data[i,:,3])
-        ax2.plot(-data[i,:,6]*2*np.pi/360, data[i,:,5])
-        plt.show()
+    # Number of plots along horizontal edge
+    dim = 4
+    axs = plt.subplots(math.ceil(len(data)/dim), dim, figsize = (18, 9))
+    for i in range(n - 1):
+        axs[1][math.floor(i/dim)][i % dim].plot(data[i,:,2], data[i,:,3])
+    plt.show()
 
+# Plot the voltage of the bot as a funtion of time
 if(args.volt):
     fig = plt.figure(figsize = (12, 9))
     ax1 = fig.add_subplot(111)
     ax1.plot(measurements['cdur'][botdict[args.botnum]], measurements['voltage'][botdict[args.botnum]])
+    plt.show()
+
+# Plot trajectory of bot after trimming unwanted data points
+if(args.trimplot):
+    xnew, ynew = trimradius(data[0,:,2], data[0,:,3], data[0,:,5], 570)
+
+    fig = plt.figure(figsize = (18, 9))
+    ax1 = fig.add_subplot(121)
+    ax2 = fig.add_subplot(122)
+    ax1.plot(data[0,:,2], data[0,:,3])
+    ax2.plot(xnew, ynew)
     plt.show()
--- mylib.py
+++ mylib.py
@@ -1,12 +1,15 @@
 import os
+import math
 import argparse
 import numpy as np
 
 def arguments():
+    # Use argparse to parse commandline arguments
     parser = argparse.ArgumentParser()
-    parser.add_argument('botnum', type = str)
-    parser.add_argument('--traj', action = "store_true")
-    parser.add_argument('--volt', action = "store_true")
+    parser.add_argument('botnum', type = str, nargs = '?', default = 3)
+    parser.add_argument('--traj', action = "store_true", default = False)
+    parser.add_argument('--volt', action = "store_true", default = False)
+    parser.add_argument('--trimplot', action = "store_true", default = False)
     args = parser.parse_args()
     return args
 
@@ -35,12 +38,28 @@ def readvolts():
         headersl1 = f.readline().split(',')
         headersl2 = f.readline().split(',')
     temp = np.loadtxt(filename, delimiter = ',', skiprows = 2)
+
+    # Cycle number
     cycle = np.array(temp[:, 0]).transpose()
+
+    # Voltage measurements
     volt = np.array(temp[:, 1:13]).transpose()
+
+    # Voltage uncertainties
     vunc = np.array(temp[:, 13:25]).transpose()
+
+    # Cycle duration
     dur = np.array(temp[:, 25:37]).transpose()
+
+    # Uncertainty in cycle duration
     dunc = np.array(temp[:, 37:49]).transpose()
+
+    # Cumulative duration
     cdur = np.array(temp[:, 49:61]).transpose()
+
+    # Compile everything into a dictionary for easy access
     data = {'cycle': cycle, 'voltage': volt, 'vuncertainty': vunc, 'dur': dur, 'duncertainty': dunc, 'cdur': cdur}
+
+    # Create a dictionary to associate bot numbers with order in list
     botdict = {"3": 0, "5": 1, "7": 2, "10": 3, "14": 4, "15": 5, "16": 6, "17": 7, "19": 8, "?": 9, "B": 10, "D": 11}
     return headersl1, headersl2, data, botdict
